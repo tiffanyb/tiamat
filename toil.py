86fa507e959ed3d44c39615f382a5e23945885c7
Matthew Maurer <maurer@matthewmaurer.org>
2014-09-04 12:18:29 -0400

    Added disassembly + fallthrough address

--- toil.py
+++ toil.py
@@ -12,18 +12,26 @@ class ToIL():
              ,Premise("word128",   [Bind("fileName", "string")
                                    ,Bind("addr",     "addr")
                                    ,Bind("data",     "blob")])]
-  conclusions = [ConcTy("hasil", ["string", "addr", "json"])]
+  conclusions = [ConcTy("hasil", ["string", "addr", "json"]),
+                 ConcTy("fallthrough", ["string", "addr", "addr"]),
+                 ConcTy("hasasm", ["string", "addr", "string"])]
   name = "ToIL"
   def analyze(self, fileName, arch, addr, data):
-    toilProc = Popen([toil, '--arch', arch, '--addr', str(addr),'--format' , 'json'], stdout=PIPE, stderr=PIPE, stdin=PIPE)
+    toilProc = Popen([toil, '--arch', arch, '--addr', str(addr),'--format' , 'json', '--dump-asm', '--dump-fallthrough'], stdout=PIPE, stderr=PIPE, stdin=PIPE)
     toilProc.stdin.write(data)
     toilProc.stdin.close()
-    jstr = toilProc.stdout.read().decode()
+    s = toilProc.stdout.read().decode()
     try:
-      out = json.loads(jstr)
-      return [Conc("hasil", [fileName, addr, out])]
+      out,pos = json.JSONDecoder().raw_decode(s)
+      rest = s[pos+1:].split('\n')
+      if rest[0] == "Assembly not available for this architecture.":
+        asm = []
+      else:
+        asm = [Conc("hasasm", [fileName, addr, rest[0]])]
+      fall = int(rest[1].split(':')[0])
+      return [Conc("hasil", [fileName, addr, out]),
+              Conc("fallthrough", [fileName, addr, fall])] + asm
     except ValueError:
+      print(s)
       print("No IL")
-      print(ctx)
-      print(jstr)
       return []
